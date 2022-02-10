36f2c80ddd25e60d93471fa1d972c26c3d2f4b0b
Derek <derek.ruggirello@gmail.com>
2022-02-10 16:44:39 -0500

    Update Ticket_3.py

--- Ticket_3.py
+++ Ticket_3.py
@@ -1,9 +1,13 @@
 # Revision number 1 9Feb2022
 ## Begin Derek Ruggirello here 9Feb2022
 
+radius = float(input("Please enter a circle radius: ")) # Variable to store radius input as a float
 
+print("Radius =", radius) # Outputs the radius input
 
+area = (3.1415926 * (radius**2)) # Calculates and stores area from the input radius
 
+print("Area =", area) # Outputs the result of the area
 
 # Revision number 1 10Feb2022
 ## End Derek Ruggirello here
