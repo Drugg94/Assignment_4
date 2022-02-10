834e3dbce4cedec70311b15360dfca7f2a387d79
Derek <derek.ruggirello@gmail.com>
2022-02-10 16:16:34 -0500

    Update Ticket_1.py

--- Ticket_1.py
+++ Ticket_1.py
@@ -1,8 +1,14 @@
 # Revision number 1 9Feb2022
 ## Begin Derek Ruggirello here 9Feb2022
 
+x = input("Please enter a string: ") # This variable stores the input.
 
+print("You entered", x, "and its type is", type(x)) # Outputs a repetition of x and its type
+
+x = input("Please enter an integer:") # Stores the input into x again
+
+print("You entered", x, "and its type is", type(x)) # Outputs a repetition of x and its type
 
 # Revision number 1 10Feb2022
 ## End Derek Ruggirello here
-# Group / manager/ lead tech/ project #
+# Omega Group/ Ram Valud/ Michael Walker/ project greenwood321 #
