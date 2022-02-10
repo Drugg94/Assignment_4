b81486a6c60c93b484b0f07b05a638cf71625410
Derek <derek.ruggirello@gmail.com>
2022-02-10 17:26:32 -0500

    Ticket 4 solution

--- Ticket_4.py
+++ Ticket_4.py
@@ -1,9 +1,17 @@
 # Revision number 1 9Feb2022
 ## Begin Derek Ruggirello here 9Feb2022
 
+character = input("Please enter a letter to be determined if it is a vowel: ") # Variable for user input
 
-
-
+# If statement in order to determine if user input is a vowel
+if character == "a" or character == "A" or character == "e" or character == "E":
+    print(character, "is a vowel!")
+elif character == "i" or character == "I" or character == "o" or character == "O":
+    print(character, "is a vowel!")
+elif character == "u" or character == "U":
+    print(character, "is a vowel!") # When user input is a vowel then this output executes
+else:
+    print(character, "is not a vowel!") # When the user output is not a vowel this output executes
 
 # Revision number 1 10Feb2022
 ## End Derek Ruggirello here
