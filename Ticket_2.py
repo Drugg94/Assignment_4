50151702be1b13ce8d938d1b79db8f5ee7597b4e
Derek <derek.ruggirello@gmail.com>
2022-02-10 16:36:43 -0500

    Many ticket entries
    Ticket 2 and solution.
    Tickets 3,4,5,6,7 Comment entries

---
+++ Ticket_2.py
@@ -0,0 +1,14 @@
+# Revision number 1 10Feb2022
+## Begin Derek Ruggirello here 10Feb2022
+
+print("Twinkle, twinkle, little star,") # Output
+print("\tHow I wonder what you are!") # Output with escape tab
+print("\t\tUp above the world so high,\n") # Output with two escape tabs
+
+print("\t\tLike a diamond in the sky.") # Output with two escape tabs
+print("Twinkle, twinkle, little star,") # Output
+print("\tHow I wonder what you are") # Output with escape tab
+
+# Revision number 1 10Feb2022
+## End Derek Ruggirello here
+# Omega Group/ Ram Valud/ Michael Walker/ project greenwood321 #
---
+++ Ticket_4.py
@@ -0,0 +1,10 @@
+# Revision number 1 9Feb2022
+## Begin Derek Ruggirello here 9Feb2022
+
+
+
+
+
+# Revision number 1 10Feb2022
+## End Derek Ruggirello here
+# Omega Group/ Ram Valud/ Michael Walker/ project greenwood321 #
